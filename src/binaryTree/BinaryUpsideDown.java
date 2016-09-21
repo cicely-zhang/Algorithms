13c5e3af0d5703da66973f222ffe70b84e011970
Cicely Zhang <ciczhang@cisco.com>
2016-09-20 23:29:41 -0400

    add java project info

---
+++ src/binaryTree/BinaryUpsideDown.java
@@ -0,0 +1,39 @@
+package binaryTree;
+
+
+public class BinaryUpsideDown {
+	
+	public TreeNode flipUpsideDown(TreeNode root) {
+		
+		TreeNode head = null;
+		flipUpsideDownHelper(root, head);
+		return head;
+	}
+	
+	private TreeNode flipUpsideDownHelper(TreeNode root, TreeNode head) {
+		if (root == null)
+			return root;
+		
+		else if (root.left == null && root.right == null) {
+			return root;
+		}
+		
+		TreeNode node1 = flipUpsideDown(root.left);
+		if (head == null)
+			head = node1;
+		
+		node1.left = root.right;
+		node1.right = root;
+		
+		return root;
+	}
+	
+	public static void main(String[] args) {
+		
+		Integer[] treeArray = new Integer[] {1,2,3,4,5};
+		
+	}
+	
+	
+
+}
---
+++ src/binaryTree/TreeNode.java
@@ -0,0 +1,14 @@
+package binaryTree;
+
+public class TreeNode {
+
+	public TreeNode left;
+	public TreeNode right;
+	public int val;
+	
+	public TreeNode(int value) {
+		this.left = null;
+		this.right = null;
+		this.val = value;
+	}
+}
