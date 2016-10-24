b0783aa6f6e08980eadd451e008bbac1b3860b4d
Cicely Zhang <ciczhang@cisco.com>
2016-10-24 10:16:08 -0400

    working trapping rain water

--- src/linear/TrappingRainWater.java
+++ src/linear/TrappingRainWater.java
@@ -3,26 +3,38 @@ package linear;
 public class TrappingRainWater {
 	
 	public static int trap(int[] height) {
-		int left = 0;
-        int right = left + 1;
-        int sum = 0;
-        int potentialHold = 0;
-        while (right < height.length) {
+		if (height == null || height.length == 0 || 
+				height.length == 1 || height.length == 2) {
+            return 0;
+        }
+        int l = 0;
+        int r = height.length - 1;
+        int leftWall = height[l];
+        int rightWall = height[r];
+        int trapped = 0;
+        
+        while (l < r) {
             
-            if (height[right] < height[left]) {
-                potentialHold += height[left] - height[right];
+            if (leftWall < rightWall) {
+                l ++;
+                if (height[l] < leftWall) 
+                    trapped += leftWall - height[l];
+                else
+                    leftWall = height[l];
             }
             else {
-                sum += potentialHold;
-                left = right;
+                r --;
+                if (height[r] < rightWall)
+                    trapped += rightWall - height[r];
+                else 
+                    rightWall = height[r];
             }
-            right += 1;
         }
-        return sum;
+        return trapped;
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(TrappingRainWater.trap(new int[] {4,2,3}));
+		System.out.println(TrappingRainWater.trap(new int[] {}));
 	}
 
 }
